584982e75d51f0ec9df2a39a166ee667e50bb346
subhasmitamishra310 <63075907+subhasmitamishra310@users.noreply.github.com>
2020-05-03 00:47:27 -0700

    yess

---
+++ src/main/java/org/oar/git/CheckPerfectSquare.java
@@ -0,0 +1,18 @@
+package org.oar.git;
+
+import java.util.Scanner;
+
+public class CheckPerfectSquare {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter no to check");
+		int n=sc.nextInt();
+		for(int i=0;i<n/2;i++)
+		{
+			if(i*i==n);	
+		}
+		System.out.println("yes this is a perfect sqr");
+	}
+
+}
